3af05f6ca4937119e78eb902d391b85084c64dc4
Sayef Sarkar <sayefeshan7@gmail.com>
2025-06-23 20:22:15 +0600

    volume issue fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://gateway:80",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
